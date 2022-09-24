8a4d7a8e6af4a30835a8df04e8a9f2bd0073278a
viola134 <violapakhomova13@gmail.com>
2022-09-24 16:43:46 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$t = 0;
+print "<table style=\"border:solid;\">";
+for ($i = 20; $i > -20; $i--) {
+    $style = $t > $i ? 'width:30px; background:red' : 'width:30px; background:blue';
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"{$style}\"\></td></tr>";
+}
